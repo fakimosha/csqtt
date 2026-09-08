62fefddcfe9bdd6876906b078a2707417826d460
fakimosha <fakimosha@mail.ru>
2026-09-08 12:13:34 +0300

    Update memory_metrics.rs

--- rust-server/memory_metrics.rs
+++ rust-server/memory_metrics.rs
@@ -194,6 +194,6 @@ pub fn available_memory_bytes() -> Option<u64> {
     None
 }
 
-pub fn collect_allocator_thread_heap() -> serde_json::Value {
-    serde_json::Value::Null
+pub fn collect_allocator_thread_heap() -> bool {
+    false
 }
